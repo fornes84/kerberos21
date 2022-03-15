d57bd0ce305e911f2e3597c118849862fd861187
marc <marc@pcmarc.pcmarc>
2022-03-15 22:56:45 +0100

    algo no va

--- kerberos/kserver21/startup.sh
+++ kerberos/kserver21/startup.sh
@@ -4,7 +4,8 @@ cp /opt/docker/krb5.conf /etc/krb5.conf 	# Sobreescrivim els fitxers per els nos
 cp /opt/docker/kdc.conf /etc/krb5kdc/kdc.conf
 cp /opt/docker/kadm5.acl /etc/krb5kdc/kadm5.acl
 
-kdb5_util create -s -P masterkey
+kdb5_util create -s -P masterkey 
+#??
 #kadmin.local -q "addprinc -pw kalpaca alpaca"
 #kadmin.local -q "addprinc -pw kpere pere"
 #kadmin.local -q "addprinc -pw kmarta marta"
